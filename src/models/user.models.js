6a331bd8b8d541d554ac85101fbfbbf202843916
Prajwal Patil <ppbaviskar7030@gmail.com>
2025-06-25 12:34:29 +0530

    identation

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -14,6 +14,7 @@ const userSchema = new Schema(
         trim: true,
         index: true
         },
+
         email: {
         type:  String,
         required: true,
@@ -21,33 +22,40 @@ const userSchema = new Schema(
         lowercase: true,
         trim: true,
         },
+
         fullName: {
         type:  String,
         required: true,
         trim: true,
         index: true
         },
+
         avatar: {
         type:  String,  //cloudinary url
         required: true
         },
+
         coverImage: {
         type:  String,
         },
+
         watchHistory: [
            { 
             type: Schema.Types.ObjectId,
             ref: "Video"
            }
         ],
+
         password: {
             type: String,
             required: [true, "Password is required!!"]
         },
+
         refreshToken: {
             type: String
         }
     },
+    
     {timestamps: true}
 )
 
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -9,34 +9,42 @@ const videoSchema = new Schema(
         required: true,
         unique: true,
         },
+
         thumbnail: {
         type:  String,
         required: true,
         },
+
         title: {
         type:  String,
         required: true,
         },
+
         description: {
         type:  String,  //cloudinary url
         required: true
         },
+
         time: {
         type:  Number,  //cloudinary url
         },
+
         password: {
             type: Number,
             default: 0
         },
+
         isPublished: {
             type: Boolean,
             default: true
         },
+
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User"
         }
     },
+    
     {timestamps: true}
 )
 
